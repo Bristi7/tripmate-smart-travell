aa2537f7d72ea9e7baa122ae51c13864575f865a
Bristi Samadder <bristisamadder197@gmail.com>
2026-07-21 01:40:15 +0530

    Create popular-routes.js

---
+++ assets/js/popular-routes.js
@@ -0,0 +1,37 @@
+jQuery(document).ready(function($){
+    $('.popular-routes-slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 2800,
+        asNavFor: '.popular-routes-slider-nav'
+      });
+
+      $('.popular-routes-slider-nav').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.popular-routes-slider-for',
+        arrows: false,
+        dots: false,
+        centerMode: true,
+        focusOnSelect: true,
+        responsive: [
+          {
+            breakpoint: 901,
+            settings: {
+              slidesToShow: 3,
+              centerMode: true
+            }
+          },
+          {
+            breakpoint: 641,
+            settings: {
+              slidesToShow: 2,
+              centerMode: false
+            }
+          }
+        ]
+      });
+})
